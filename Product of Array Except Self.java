ad54ee618ac6ff0210baac1cdff314c1f3d7adce
Pruthviraj Rathod <pruthvirajrathod645@gmail.com>
2025-11-09 23:48:06 +0530

    Create  Product of Array Except Self.java

---
+++ Product of Array Except Self.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        // Step 1: left product build
+        result[0] = 1;
+        for(int i = 1; i < n; i++){
+            result[i] = result[i-1] * nums[i-1];
+        }
+
+        // Step 2: right product multiply
+        int right = 1;
+        for(int i = n-1; i >= 0; i--){
+            result[i] = result[i] * right;
+            right = right * nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int[] ans = productExceptSelf(arr);
+        System.out.println(Arrays.toString(ans)); // Output: [24,12,8,6]
+    }
+}
