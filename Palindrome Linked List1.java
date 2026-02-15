54720fd20c40395daa935ec93a930f4ddc9bbdc2
Pruthviraj Rathod <pruthvirajrathod645@gmail.com>
2026-02-15 22:48:41 +0530

    Create Palindrome Linked List1.java

---
+++ Palindrome Linked List1.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        
+        // Step 1: Find the middle of the linked list
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // Step 2: Reverse the second half of the list
+        ListNode secondHalfHead = reverse(slow.next);
+        
+        // Step 3: Compare the first half with the reversed second half
+        ListNode p1 = head;
+        ListNode p2 = secondHalfHead;
+        boolean isPalindrome = true;
+        while (p2 != null) { // Only need to compare the second half
+            if (p1.val != p2.val) {
+                isPalindrome = false;
+                break;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        
+        // Step 4: Restore the list (optional)
+        slow.next = reverse(secondHalfHead);
+        
+        return isPalindrome;
+    }
+    
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode nextTemp = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextTemp;
+        }
+        return prev;
+    }
+}
